01d7bcb63679a654bdee58498096ce355623461c
mdachi-codes <jessejustinm@gmail.com>
2019-12-02 14:18:27 +0300

    fix: resloved '//' issue on defaultAction by updating menu module to version 1.0.11-beta.6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NewComponent } from './new/new.component';
-import { DashComponent } from './dash/dash.component';
-import { KeyComponent } from './key/key.component';
-import { ImportComponent } from './import/import.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { NewComponent } from "./new/new.component";
+import { DashComponent } from "./dash/dash.component";
+import { KeyComponent } from "./key/key.component";
+import { ImportComponent } from "./import/import.component";
 
 const routes: Routes = [
-  { path: "", redirectTo:"home", pathMatch:"full" },
+  { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "new", component: NewComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'import', component: ImportComponent },
-  { path: "namespace/:name", component: DashComponent, children:[
-    { path: ":key", component: KeyComponent }
-  ]
- 
- },
-  
+  { path: "home", component: HomeComponent },
+  { path: "import", component: ImportComponent },
+  {
+    path: "namespace/:name",
+    component: DashComponent,
+    children: [{ path: ":key", component: KeyComponent }]
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
